93c9ca1f0224f580d87d3b46e54a1a4bda31478a
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-18 16:53:27 -0500

    form component set up. need to add code about form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,15 @@
 import { Component } from "react";
 
 class Form extends Component {
+    constructor(){
+        super();
+    }
 
+    render(){
+        return(
+            
+        )
+    }
 }
 
 export default Form;
